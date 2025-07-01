5e6f0a02bf8bd738dfc9082731139c36c8bbca56
Jackson Denis <jackson.denis@live.com>
2025-06-30 22:20:45 -0300

    feito as funçoes de validar cpf , convertersegundos, tabuada e criada uma classe que são usadas funçoes em comum

---
+++ src/EntradaDeDados.java
@@ -0,0 +1,44 @@
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class EntradaDeDados {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    public static int lerInteiroSafely(String s) {
+        while (true) {
+            try {
+                System.out.print(s);
+                return SCANNER.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Tente novamente.");
+                SCANNER.nextLine(); // descarta entrada inválida
+            }
+        }
+    }
+
+    public static double lerDoubleSafely(String s) {
+        while (true) {
+            try {
+                System.out.print(s);
+                return SCANNER.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Tente novamente.");
+                SCANNER.nextLine(); // descarta entrada inválida
+            }
+        }
+    }
+
+    public static void encerrarAplicacao(List<String> HISTORICO) {
+        System.out.println("Encerrando a aplicação...");
+        if (!HISTORICO.isEmpty()) {
+            System.out.println("Histórico de cálculos:");
+            for (String registro : HISTORICO) {
+                System.out.println(registro);
+            }
+        } else {
+            System.out.println("Nenhum cálculo foi realizado.");
+        }
+        SCANNER.close();
+        System.exit(0);
+    }
+}
