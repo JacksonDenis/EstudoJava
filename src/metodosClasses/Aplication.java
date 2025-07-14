fd8f2619c953106289480dda27a1ffbc5f29d960
Jackson Denis <jackson.denis@live.com>
2025-07-13 23:21:50 -0300

    Inicio dos exercicios de classe e metodos

---
+++ src/metodosClasses/Aplication.java
@@ -0,0 +1,8 @@
+package metodosClasses;
+
+public class Aplication {
+    public static void main(String[] args) {
+        Retangulo retangulo = new Retangulo(5, 10);
+        System.out.println(retangulo);
+    }
+}
